fade46633d339829938f10001efce84311a061b6
jushack <3.jushack.1@gmail.com>
2022-03-14 19:31:54 +0900

    practice-1

--- 기본편/practice1.py
+++ 기본편/practice1.py
@@ -2,12 +2,13 @@ import sys
 print("python", "java", file=sys.stdout)
 print("python", "java", file=sys.stderr)
 
-#시험성적
-scores = {"수학":0, "영어":50, "코딩":100}
+# 시험성적
+scores = {"수학": 0, "영어": 50, "코딩": 100}
 for subjects, score in scores.items():
     print(subjects.ljust(3), str(score).rjust(3), sep=" : ")
 
-#은행대기순번
-#001 002 003 ...
+
+# 은행대기순번
+# 001 002 003 ...
 for num in range(1, 21):
     print("대기번호 : " + str(num).zfill(3))
